08985d6a1271de066c92a949c3db84c91f4a6093
Jalon <guojinloong@163.com>
2018-08-10 21:22:10 +0800

    first commit

---
+++ ImageProcess/Resource.h
@@ -0,0 +1,102 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ImageProcess.rc
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_POPUP_EDIT                  119
+#define ID_STATUSBAR_PANE1              120
+#define ID_STATUSBAR_PANE2              121
+#define IDS_STATUS_PANE1                122
+#define IDS_STATUS_PANE2                123
+#define IDS_TOOLBAR_STANDARD            124
+#define IDS_TOOLBAR_CUSTOMIZE           125
+#define ID_VIEW_CUSTOMIZE               126
+#define IDR_MAINFRAME                   128
+#define IDR_MAINFRAME_256               129
+#define IDR_ImageProcessTYPE            130
+#define IDS_EDIT_MENU                   306
+#define ID_32771                        32771
+#define ID_32772                        32772
+#define ID_FILTER_BOX                   32773
+#define ID_32774                        32774
+#define ID_FILTER_MEAN                  32775
+#define ID_32776                        32776
+#define ID_FILTER_GAUSSIAN              32777
+#define ID_32778                        32778
+#define ID_FILTER_MEDIAN                32779
+#define ID_32780                        32780
+#define ID_FILTER_BILATERAL             32781
+#define ID_32784                        32784
+#define ID_32785                        32785
+#define ID_PROCESS_FILTER_BOX           32786
+#define ID_PROCESS_FILTER_MEAN          32787
+#define ID_PROCESS_FILTER_GAUSSIAN      32788
+#define ID_PROCESS_FILTER_MEDIAN        32789
+#define ID_PROCESS_FILTER_BILATERAL     32790
+#define ID_PROCESS_MORPHOLOGY_DILATE    32791
+#define ID_32792                        32792
+#define ID_S_MORPHOLOGY_ERODE           32793
+#define ID_32794                        32794
+#define ID_S_MORPHOLOGY_OPEN            32795
+#define ID_32796                        32796
+#define ID_S_MORPHOLOGY_CLOSE           32797
+#define ID_32798                        32798
+#define ID_S_MORPHOLOGY_GRADIENT        32799
+#define ID_32800                        32800
+#define ID_S_MORPHOLOGY_TOPHAT          32801
+#define ID_32802                        32802
+#define ID_S_MORPHOLOGY_BLACKHAT        32803
+#define ID_PROCESS_MORPHOLOGY_ERODE     32805
+#define ID_PROCESS_MORPHOLOGY_OPEN      32806
+#define ID_PROCESS_MORPHOLOGY_CLOSE     32807
+#define ID_PROCESS_MORPHOLOGY_GRADIENT  32808
+#define ID_PROCESS_MORPHOLOGY_TOPHAT    32809
+#define ID_PROCESS_MORPHOLOGY_BLACKHAT  32810
+#define ID_32812                        32812
+#define ID_32813                        32813
+#define ID_32814                        32814
+#define ID_PROCESS_FLOODFILL            32815
+#define ID_PROCESS_PYRAMID              32816
+#define ID_32817                        32817
+#define ID_32818                        32818
+#define ID_THRESHOLD                    32819
+#define ID_THRESHOLD_ADAPTIVE           32820
+#define ID_32821                        32821
+#define ID_32822                        32822
+#define ID_32823                        32823
+#define ID_32824                        32824
+#define ID_32825                        32825
+#define ID_PROCESS_THRESHOLD            32827
+#define ID_PROCESS_THRESHOLD_ADAPTIVE   32828
+#define ID_32829                        32829
+#define ID_32830                        32830
+#define ID_32831                        32831
+#define ID_32832                        32832
+#define ID_TRANSFORM_EDGE_CANNY         32833
+#define ID_TRANSFORM_EDGE_SOBEL         32834
+#define ID_TRANSFORM_EDGE_LAPLACIAN     32835
+#define ID_TRANSFORM_EDGE_SCHARR        32836
+#define ID_32837                        32837
+#define ID_TRANSFORM_HOUGH_HOUGHLINES   32838
+#define ID_32839                        32839
+#define ID_TRANSFORM_HOUGH_HOUGHLINESP  32840
+#define ID_32841                        32841
+#define ID_TRANSFORM_HOUGH_HOUGHCIRCLES 32842
+#define ID_32845                        32845
+#define ID_TRANSFORM_AFFINE_WARP        32846
+#define ID_32847                        32847
+#define ID_TRANSFORM_AFFINE_ROTATION    32848
+#define ID_TRANSFORM_REMAP              32849
+#define ID_TRANSFORM_EQUALIZEHIST       32850
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        311
+#define _APS_NEXT_COMMAND_VALUE         32851
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
